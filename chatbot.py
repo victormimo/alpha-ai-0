c330091679efa32575c79eb79d4d1b9dde8f3c06
Victor Mimo <victormimoc@gmail.com>
2023-12-14 23:39:10 -0500

    updating with truval

--- chatbot.py
+++ chatbot.py
@@ -11,6 +11,25 @@ from llama_index import SimpleDirectoryReader
 from llama_index import Document
 from llama_index import VectorStoreIndex
 from llama_index import ServiceContext
+import datetime
+from trulens_eval import (
+    Tru,
+    Feedback,
+    TruLlama,
+    OpenAI
+)
+from trulens_eval.feedback import Groundedness
+
+
+
+st.set_page_config(
+    page_title="Chat with Alpha AI",
+    page_icon="",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items=None,
+)
+
 
 # Load the OpenAI key from the environment variable
 openai_key = os.getenv("OPENAI_KEY")
@@ -18,6 +37,9 @@ if not openai_key:
     st.error("No OpenAI key found. Please set the OPENAI_KEY environment variable.")
 
 openai.api_key = openai_key
+
+llm = OpenAI(model="gpt-4", temperature=0.1, system_prompt=system_prompt)
+
 
 
 # This function will load the vector store index
@@ -29,7 +51,6 @@ def load_vector_store_index():
 
     document = Document(text="\n\n".join([doc.text for doc in documents]))
 
-    llm = OpenAI(model="gpt-4", temperature=0.1, system_prompt=system_prompt)
     service_context = ServiceContext.from_defaults(
         llm=llm, embed_model="local:BAAI/bge-small-en-v1.5"
     )
@@ -40,10 +61,50 @@ def load_vector_store_index():
 index=load_vector_store_index()
 query_engine = index.as_query_engine()
 print(query_engine)
+print("timestamp")
+print(datetime.datetime.now())
 
-response = query_engine.query(
-    "What are steps to take when finding projects to build your experience?"
-)
+@st.cache_data
+def load_trulens():
+    tru = Tru()
+    tru.reset_database()
+
+    qa_relevance = (
+        Feedback(openai.relevance_with_cot_reasons, name="Answer Relevance")
+        .on_input_output()
+    )
+
+    qs_relevance = (
+        Feedback(openai.relevance_with_cot_reasons, name = "Context Relevance")
+        .on_input()
+        .on(TruLlama.select_source_nodes().node.text)
+        .aggregate(np.mean)
+    )
+
+    #grounded = Groundedness(groundedness_provider=openai, summarize_provider=openai)
+    grounded = Groundedness(groundedness_provider=openai)
+
+    groundedness = (
+        Feedback(grounded.groundedness_measure_with_cot_reasons, name="Groundedness")
+            .on(TruLlama.select_source_nodes().node.text)
+            .on_output()
+            .aggregate(grounded.grounded_statements_aggregator)
+    )
+
+    feedbacks = [qa_relevance, qs_relevance, groundedness]
+
+    tru_recorder = TruLlama(
+        query_engine,
+        app_id="Direct Query Engine",
+        feedbacks=feedbacks
+        )
+    return tru_recorder
+
+# response = query_engine.query(
+#     "What are steps to take when finding projects to build your experience?"
+# )
+
+# print(str(response))
 
 domain_headers = {
     "api.openai.com": {
@@ -55,13 +116,6 @@ domain_headers = {
     },
 }
 
-st.set_page_config(
-    page_title="Chat with Alpha AI",
-    page_icon="",
-    layout="centered",
-    initial_sidebar_state="auto",
-    menu_items=None,
-)
 
 
 st.title("Alpha AI 0")
@@ -93,30 +147,23 @@ for message in st.session_state.messages:  # Display the prior chat messages
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
+
 # If last message is not from assistant, generate a new response
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.spinner("The AI is thinking..."):
-        response: StreamingAgentChatResponse = st.session_state.chat_engine.stream_chat(
-            prompt,
-            [
-                ChatMessage(role=m["role"], content=m["content"])
-                for m in st.session_state.messages
-            ],
-        )
-        # while there processing and no response, keep showing the spinner
-        while not response._is_done and response._queue.empty():
-            continue
+         with tru_recorder as recording:
+            # Use query_engine to process the prompt
+            vector_response = query_engine.query(prompt)
+       
 
     with st.chat_message("assistant"):
-        full_response = ""
-        message_placeholder = st.empty()
-        for response_chunk in response.response_gen:
-            # Update the full response and the placeholder with the received chunk
-            full_response += response_chunk
-            message_placeholder.markdown(full_response + "▌")
+      
+        # Display the full response in the chat
+        st.markdown(vector_response)
 
-        # Update the placeholder with the final response
-        message_placeholder.markdown(full_response)
+        # Append the response to the session state messages
         st.session_state.messages.append(
-            {"role": "assistant", "content": full_response}
+            {"role": "assistant", "content": vector_response}
         )
+
+
